7f7b348a3d6b0a8a3f41c970531edeb0578e021d
bdkelly <benjaminkelly97@gmail.com>
2020-10-19 17:54:18 -0400

    Create rollingWindow.py

---
+++ src/work/rollingWindow.py
@@ -0,0 +1,4 @@
+def rolling_window(a, window, step_size):
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1 - step_size + 1, window)
+    strides = a.strides + (a.strides[-1] * step_size,)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
